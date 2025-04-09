143e18043934fd0ab70de6b8a26af67281ad5b4e
Jessica <jkp.hr2309@gmail.com>
2025-04-09 21:45:12 +0200

    Agregando enrutador para varias paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -10,18 +11,14 @@ function App() {
     <div className="container mt-5">
       <div className="text-center">
         <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" /></a>
         <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1 className="text-primary my-4">¡Mi Proyecto Real!</h1>
       <div className="card p-4 shadow">
-        <button
-          className="btn btn-primary mb-3"
-          onClick={() => setCount(count + 1)}
-        >
+        <button className="btn btn-primary mb-3" onClick={() => setCount(count + 1)}>
           Clics: {count}
         </button>
         <p>
