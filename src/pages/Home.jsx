58e5ea0e65ed37f64267c426221e7adda8054395
Cynthia Carino <cynthia.carinot@gmail.com>
2025-05-07 19:35:23 +0200

    Inicio de la pagina

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+function Home() {
+  return (
+    <div className="container text-center my-5">
+      <h1 className="display-4 animate__animated animate__fadeIn">Bienvenidos a Sabor Salvadoreño y Mexicano
+      </h1>
+      <p className="lead animate__animated animate__fadeIn animate__delay-1s">
+        Disfruta de los auténticos sabores de El Salvador y México...
+      </p>
+      <a href="/menu" className="btn btn-primary btn-lg mt-3 animate__animated animate__fadeIn animate__delay-2s">
+        Ver el Menú
+      </a> </div>
+  );
+}
+export default Home;
