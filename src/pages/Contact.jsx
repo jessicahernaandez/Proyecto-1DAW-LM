d3e44f97b2a4466ff1dc3e6918e003746e197ed7
Cynthia Carino <cynthia.carinot@gmail.com>
2025-05-08 01:41:15 +0200

    Pagina de contacto

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+
+
+function Contact() {
+    return (
+      <div className="container my-5">
+        
+        <h2 className="text-center mb-4 animate__animated animate__fadeIn">Contáctanos</h2>
+      <div className="animate__animated animate__fadeIn animate__delay-1s">
+        <p><strong>Dirección:</strong> Calle Principal 123...</p>
+        <p><strong>Teléfono:</strong> +503 1234-5678</p>
+        {/* Otros datos */}
+      </div>
+    </div>
+  );
+}
