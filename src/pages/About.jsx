df3431327c4f3eeeb7af6b0aef7b61fd3848546b
Cynthia Carino <cynthia.carinot@gmail.com>
2025-05-08 01:36:12 +0200

    Pagina de contacto

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+
+
+function About() {
+    return (
+    <div className="container my-5">
+    <h2 className="text-center mb-4 animate__animated animate__fadeIn">Acerca de Nosotros</h2>
+    <p className="animate__animated animate__fadeIn animate__delay-1s">
+    Sabor Salvadoreño y Mexicano nació de la pasión...
+    </p>
+    <p className="animate__animated animate__fadeIn animate__delay-2s">
+Nuestra misión es ofrecer una experiencia...
+</p>
+</div>
+);
+}
