3b220ab5f68b83a600a6bad7c18d6a9116054b35
Jessica <jkp.hr2309@gmail.com>
2025-05-08 16:32:35 +0200

    Añadi _redirects para enrutamiento en Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './CSS/App.css'; // Se ha modificado la ruta para importarla.
 /*Importaciones de las paginas creadas.*/
-//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import About from './pages/About';
@@ -18,17 +18,7 @@ function App() {
   return (
 
     <Router basename="/">
-      {/*Definición de rutas*/}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/reservations" element={<Reservations />} />
-          <Route path="/contact" element={<Contact />} />
-       </Routes>
-      
-
-      {/*Barra de navegación con Bootstrap*/}
+    {/*Barra de navegación con Bootstrap*/}
       <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <div className="container">
           <Link className="navbar-brand" to="/">Sabor Salvadoreño y Mexicano</Link>
@@ -56,6 +46,15 @@ function App() {
           </div>
         </div>
       </nav>
+
+      {/*Definición de rutas*/}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/contact" element={<Contact />} />
+       </Routes>
 
       {/* Contador del ejemplo que viene predefinido al descargar Vite y React (Decidimos dejarlo para la prueba)*/}
       <div className="text-center my-5">
