b318cc771ea49d7b25f74eafc96b086ba048ae11
Jessica <jkp.hr2309@gmail.com>
2025-05-08 13:35:22 +0200

    Restaurar imagenes en build y corregir rutas

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Proyecto-1DAW-LM/', //Para que pueda funcionar GitHub Pages
-  build: {
-    rollupOptions: {
-      external: ['/Proyecto-1DAW-LM/*'], //Ignorar estas rutas como módulos
-    },
-  },
+  publicDir: 'public', //Asegura que Vite copie los archivos de public/
 });
