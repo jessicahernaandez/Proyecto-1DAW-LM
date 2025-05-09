f04da5f00ad1e003738ee8b6f4e396860ba3ee1f
Jessica <jkp.hr2309@gmail.com>
2025-05-09 12:09:30 +0200

    Cambios de estilos y nombres de contendores en Home.jsx y App.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="container text-center my-5">
-      <h1 className="display-4 animate__animated animate__fadeIn">Bienvenidos a Sabor Salvadoreño y Mexicano
-      </h1>
-      <p className="lead animate__animated animate__fadeIn animate__delay-1s">
-        Disfruta de los auténticos sabores de El Salvador y México...
-      </p>
-      <a href="/menu" className="btn btn-primary btn-lg mt-3 animate__animated animate__fadeIn animate__delay-2s">
-        Ver el Menú
-      </a> 
+    <div className="contenedor-principal">
+      <div className="contenedor-imagen-titulo">
+        <h1 className="titulo-principal animate__animated animate__fadeIn">Bienvenidos a Sabor Salvadoreño y Mexicano</h1>
+        <p className="pequeña-intro animate__animated animate__fadeIn animate__delay-1s">
+          Disfruta de los auténticos sabores de El Salvador y México...
+        </p>
+      </div>
+      <div className="contenedor-boton">
+        <a href="/menu" className="boton-personalizado animate__animated animate__fadeIn animate__delay-2s">
+          Ver el Menú
+        </a>
+      </div>
     </div>
   );
 }
